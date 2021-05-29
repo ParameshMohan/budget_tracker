c9e1974b954462ca7e498b5e9159dee78d420a05
Paramesh Mohan <paramesh.mohan@iga.gov.bh>
2021-05-29 17:45:46 +0300

    edit button done

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,13 +1,64 @@
-import React,{ useContext } from "react";
+import React,{ useContext,useState } from "react";
 import {AppContext} from '../context/AppContext';
+// import ViewBudget from './ViewBudget';
+// import EditBudget from './EditBudget';
+
 
 const Budget = () => {
   // we are using destructing and assigning state to a variable called budget here
-  const { budget } = useContext(AppContext);
+  const { budget, dispatch } = useContext(AppContext);
 
+  const [isEditing, setIsEditing] = useState(false)
+  const [value, setValue] = useState(budget);
+
+  const handleSaveClick = () =>{
+    dispatch({
+      type:'SET_BUDGET',
+      payload:value
+    })
+    setIsEditing(false)
+  }
+
+  const handleEdit =() =>{
+    setIsEditing(true)
+  }
   return (
-    <div className="alert alert-secondary">
+    <div className="alert alert-secondary align-items-center p-3 d-flex justify-content-between">
+
+    { isEditing ? (
+<>
+      <input
+      type='number'
+      id='name'
+			value={value}
+      onChange={(event) => setValue(event.target.value)}
+      >
+      </input>
+
+      <button
+      type='button'
+      class='btn btn-primary'
+      onClick={() => handleSaveClick(value)}>
+        Save
+      </button>
+      </>
+    ):
+    (
+
+    <>
       <span>Budget: ₹{budget}</span>
+      <button
+      type='button'
+      class='btn btn-primary'
+      onClick={() => handleEdit()}>
+       Edit
+      </button>
+      </>
+   
+    )
+}
+   
+      {/* <span>Budget: ₹{budget}</span> */}
     </div>
   );
 };
---
+++ src/components/EditBudget.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const EditBudget =(props) => {
+const [value,setValue]=useState(props.budget);
+
+return(
+    <>
+    <input
+    >
+    </input>
+    </>
+)
+}
