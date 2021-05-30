eef3b30a7e26f63d9c2bad5ad9c4f6c8757edfee
Paramesh Mohan <paramesh.mohan@iga.gov.bh>
2021-05-30 08:10:46 +0300

    complete the edit search text box in expenses

--- src/components/ExpensesList.js
+++ src/components/ExpensesList.js
@@ -1,14 +1,47 @@
-import React,{ useContext } from 'react';
+import React,{ useContext,useState ,useEffect} from 'react';
 import ExpenseItem from './ExpenseItem';
 import { AppContext } from '../context/AppContext';
 
 const ExpensesList = () =>{
 
-    const { expenses } =useContext(AppContext)
- 
+    const { expenses } =useContext(AppContext);
+ const [search, setSearch] = useState('')
+const [exp,setExp]=useState(expenses || [] )
+useEffect(() => {
+    setExp(expenses);
+}, [expenses]);
+
+useEffect(() =>{
+    if(search ===''){
+        setExp(expenses); 
+    }
+   
+},[search])
+ const handleSearch =(event) => {
+    setSearch(event.target.value)
+   const a = exp.filter(i =>{
+        return   i.name.toLowerCase().includes(event.target.value)
+    } ) 
+
+       console.log("a",a)
+       setExp(a)
+  
+ }
     return(
         <ul className='list-group'>
-            {expenses.map((expense) =>(
+
+<input
+        id='search'
+        type='text'
+ 
+        // onChange={(event) => {
+        //     // setSearch(event.target.value)
+        //     handleSearch(event.target.value) }}
+        onChange={handleSearch}
+        >
+        
+        </input>
+            {exp.map((expense) =>(
                 <ExpenseItem 
                 id={expense.id} 
                 name={expense.name} 
